b3cb923ab493c696bc704a91211c4de7ef90b361
Elian Ibaj <elianibaj@gmail.com>
2022-07-13 19:32:44 +0200

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import { Container, Todo, Hero, Quote } from './BaseStyles'
 import ProductsSection from './ProductsSection'
 
 function App() {
   return (
     <div>
-      <Todo smPadding="none">TODO: Nav</Todo>
-
-      <Hero />
-
       <ProductsSection />
-
-      <Todo>TODO: Showcase</Todo>
-
-      <Container>
-        <Quote />
-      </Container>
     </div>
   )
 }
